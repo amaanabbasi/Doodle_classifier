b02b6c5d0007091a7c844f4615305efb4a7675fc
amaan <amaanabbasi99@gmail.com>
2018-03-16 11:50:59 +0530

    Add files via upload

--- train.py
+++ train.py
@@ -1,9 +1,4 @@
-'''Trains a simple convnet on the MNIST dataset.
-Gets to 99.25% test accuracy after 12 epochs
-(there is still a lot of margin for parameter tuning).
-16 seconds per epoch on a GRID K520 GPU.
-'''
-#python 2/3 compatibility
+
 from __future__ import print_function
 #simplified interface for building models 
 import keras
@@ -15,27 +10,46 @@ from keras.models import Sequential
 #into respective layers
 from keras.layers import Dense, Dropout, Flatten
 #for convolution (images) and pooling is a technique to help choose the most relevant features in an image
-from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Conv2D, MaxPooling2D, MaxPooling2D, Conv1D
 from keras import backend as K
+import numpy as np
+import os
+
+
 
 #mini batch gradient descent ftw
 batch_size = 128
 #10 difference characters
-num_classes = 10
+num_classes = 3
 #very short training time
-epochs = 12
+epochs = 10
 
 # input image dimensions
 #28x28 pixel images. 
 img_rows, img_cols = 28, 28
 
+
+# In[3]:
+
+
 # the data downloaded, shuffled and split between train and test sets
 #if only all datasets were this easy to import and format
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+import pandas as pd
+from sklearn.model_selection import train_test_split
+X = np.load('dataset.npy')
+X = X.reshape(3000,28,28)
+y = np.load('labels.npy')
+y = np.array(y, dtype='uint8')
+y = keras.utils.np_utils.to_categorical(y)
+x_train, x_test, y_train, y_test = train_test_split(X, y)
 
-#this assumes our data format
-#For 3D data, "channels_last" assumes (conv_dim1, conv_dim2, conv_dim3, channels) while 
-#"channels_first" assumes (channels, conv_dim1, conv_dim2, conv_dim3).
+
+# In[4]:
+
+
+# this assumes our data format
+# For 3D data, "channels_last" assumes (conv_dim1, conv_dim2, conv_dim3, channels) while 
+# "channels_first" assumes (channels, conv_dim1, conv_dim2, conv_dim3).
 if K.image_data_format() == 'channels_first':
     x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
     x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
@@ -46,8 +60,7 @@ else:
     print(x_train.shape[0])
 
     x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-    input_shape = (img_rows, img_cols, 1)
-
+input_shape = (img_rows, img_cols, 1)
 #more reshaping
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
@@ -57,9 +70,10 @@ print('x_train shape:', x_train.shape)
 print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
 
-# convert class vectors to binary class matrices
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
+
+
+# In[5]:
+
 
 #build our model
 model = Sequential()
@@ -99,11 +113,17 @@ print('Test loss:', score[0])
 print('Test accuracy:', score[1])
 
 
+# In[6]:
+
+
 #Save the model
 # serialize model to JSON
 model_json = model.to_json()
+os.chdir("../Main")
 with open("model.json", "w") as json_file:
     json_file.write(model_json)
 # serialize weights to HDF5
 model.save_weights("model.h5")
 print("Saved model to disk")
+print('go to main app')
+
